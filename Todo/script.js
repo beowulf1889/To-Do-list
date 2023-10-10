18537b613344be820cde12a094fc8de76c948250
beowulf1889 <anthonyrasband2939@mtec.edu.stu>
2023-10-10 09:10:58 -0600

    added input feilds

--- Todo/script.js
+++ Todo/script.js
@@ -1,7 +1,6 @@
-const ToDo = document.getElementById("ToDo");
-const tasks = document.getElementById("Tasks");
-const header =document.getElementById("header")
-
+// Get the ToDo element by its id
+const ToDo = document.getElementById("make-task");
+const tasksContainer = document.getElementById("Tasks");
 
 ToDo.addEventListener('dblclick', function(event) {
     // Create a new input element
@@ -13,12 +12,17 @@ ToDo.addEventListener('dblclick', function(event) {
     // Handle keydown event to detect Enter key press
     input.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
-            // Create a new item with the input value
-            const newItem = document.createElement('li');
-            newItem.textContent = this.value;
+            // Generate a random ID for the task
+            const taskId = generateRandomId();
 
-            // Add the new item to the list
-            tasks.appendChild(newItem);
+            // Create a new div element for the task with the generated ID
+            const taskDiv = document.createElement('div');
+            taskDiv.className = 'tasks'; // Apply CSS class for styling
+            taskDiv.textContent = this.value;
+            taskDiv.id = taskId;
+
+            // Append the new task div to the tasks container
+            tasksContainer.appendChild(taskDiv);
 
             // Remove the input field and restore the original container text
             ToDo.removeChild(input);
@@ -30,15 +34,21 @@ ToDo.addEventListener('dblclick', function(event) {
     document.addEventListener('click', function(event) {
         if (event.target !== input) {
             ToDo.removeChild(input);
-            ToDo.textContent = 'Double click here to create a new item:';
+            
         }
     });
 
     // Replace the ToDo content with the input field
     this.innerHTML = '';
     this.appendChild(input);
-
+s
     // Focus the input field
     input.focus();
     event.stopPropagation(); // Prevent click event from being triggered immediately
 });
+
+// Function to generate a random ID for tasks
+function generateRandomId() {
+    return 'task-' + Math.random().toString(36).substr(2, 9);
+}
+
