174554e6e7af0d35aeeae903931402c89ffc553e
beowulf1889 <anthonyrasband2939@mtec.edu.stu>
2023-10-12 08:34:02 -0600

    added todo added and deletetion

--- Todo/script.js
+++ Todo/script.js
@@ -1,54 +1,90 @@
-// Get the ToDo element by its id
-const ToDo = document.getElementById("make-task");
-const tasksContainer = document.getElementById("Tasks");
-
-ToDo.addEventListener('dblclick', function(event) {
-    // Create a new input element
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.style.color = 'white'; // Set text color to white
-    input.style.background = 'black'; // Set background to black
-
-    // Handle keydown event to detect Enter key press
-    input.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-            // Generate a random ID for the task
-            const taskId = generateRandomId();
-
-            // Create a new div element for the task with the generated ID
-            const taskDiv = document.createElement('div');
-            taskDiv.className = 'tasks'; // Apply CSS class for styling
-            taskDiv.textContent = this.value;
-            taskDiv.id = taskId;
-
-            // Append the new task div to the tasks container
-            tasksContainer.appendChild(taskDiv);
-
-            // Remove the input field and restore the original container text
-            ToDo.removeChild(input);
-            ToDo.textContent = 'Double click here to create a new item:';
-        }
+document.addEventListener("DOMContentLoaded", function() {
+    const makeTaskButtons = document.getElementsByClassName("button");
+  
+    Array.from(makeTaskButtons).forEach(function(button) {
+      button.addEventListener("click", function() {
+        // Create a new to-do list container
+        const newToDoContainer = document.createElement("div");
+        newToDoContainer.className = "w-1/4 To-Do bg-slate-700 text-white rounded-md m-5 p-4";
+  
+        // Create header input element
+        const headerInput = document.createElement("input");
+        headerInput.id = "header";
+        headerInput.className = "flex bg-slate-700 text-xl rounded-md justify-center text-center mb-4 w-full";
+        newToDoContainer.appendChild(headerInput);
+        headerInput.setAttribute("placeholder", "To-Do");
+  
+        // Create "Delete" button
+        const deleteButton = document.createElement("div");
+        deleteButton.className = "delete-button bg-red-500 focus:outline-none block w-full text-center mb-2 p-2 hover:-translate-y-1 duration-300 rounded-md";
+        deleteButton.innerText = "";
+        newToDoContainer.appendChild(deleteButton);
+  
+        // Create "Make Task" button
+        const makeTaskButton = document.createElement("div");
+        makeTaskButton.className = "make-task bg-amber-400 focus:outline-none block w-full text-center hover:-translate-y-1 mb-2 p-2 hover:scale-105 duration-300 rounded-md";
+        makeTaskButton.innerText = "";
+        newToDoContainer.appendChild(makeTaskButton);
+  
+        // Create tasks container
+        const tasksContainer = document.createElement("div");
+        tasksContainer.className = "Tasks-container";
+        newToDoContainer.appendChild(tasksContainer);
+  
+        // Append the new to-do list to the body or another container
+        document.body.appendChild(newToDoContainer);
+  
+        // Event listener for adding tasks to the new to-do list
+        makeTaskButton.addEventListener("click", function() {
+          // Create a new task element
+          const newTask = document.createElement("input");
+          newTask.type = "text";
+          newTask.classList.add("tasks", "bg-neutral-800", "mb-2", "p-2", "hover:-translate-y-1", "w-full", "text-center", "rounded-md", "hover:scale-105", "duration-300");
+          newTask.setAttribute("placeholder", "New Task");
+  
+          // Add double-click event listener to the new task
+          newTask.addEventListener("dblclick", function() {
+            // Toggle Tailwind CSS classes to change background color
+            newTask.classList.toggle('bg-green-500');
+            newTask.classList.toggle('bg-neutral-800');
+          });
+  
+          // Append the new task element to the tasks container of the new to-do list
+          tasksContainer.appendChild(newTask);
+        });
+  
+        // Event listener for deleting the to-do list
+        deleteButton.addEventListener("click", function() {
+          // Remove the to-do list container from the document
+          document.body.removeChild(newToDoContainer);
+        });
+      });
     });
-
-    // Handle click event to exit the text box if clicked outside
-    document.addEventListener('click', function(event) {
-        if (event.target !== input) {
-            ToDo.removeChild(input);
-            
-        }
-    });
-
-    // Replace the ToDo content with the input field
-    this.innerHTML = '';
-    this.appendChild(input);
-s
-    // Focus the input field
-    input.focus();
-    event.stopPropagation(); // Prevent click event from being triggered immediately
-});
-
-// Function to generate a random ID for tasks
-function generateRandomId() {
-    return 'task-' + Math.random().toString(36).substr(2, 9);
+  });
+  
+// Function to delete individual task
+function deleteTask(taskElement) {
+    taskElement.remove();
 }
 
+// Function to delete all green tasks
+function deleteAllGreenTasks() {
+    const greenTasks = document.querySelectorAll('.task.bg-green-500');
+    greenTasks.forEach(task => task.remove());
+}
+
+// Event listener for deleting individual tasks using backspace key
+document.addEventListener('keydown', function(event) {
+    // Check if the pressed key is the backspace key (keyCode 8)
+    if (event.keyCode === 8) {
+        // Check if the active element has the class 'task'
+        if (document.activeElement.classList.contains('task')) {
+            deleteTask(document.activeElement);
+        }
+    }
+});
+
+// Event listener for deleting all green tasks
+document.getElementById('deleteGreenTasksButton').addEventListener('click', function() {
+    deleteAllGreenTasks();
+});
