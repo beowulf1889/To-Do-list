5f6ec4760d7392347e34b0be377e21a1738597b3
beowulf1889 <anthonyrasband2939@mtec.edu.stu>
2023-10-13 09:19:48 -0600

    added delete function for indival tasks

--- Todo/script.js
+++ Todo/script.js
@@ -1,21 +1,45 @@
+function saveToLocalStorage(id, data) {
+  localStorage.setItem(id, JSON.stringify(data));
+}
+document.addEventListener("DOMContentLoaded", () => {
+
+  Object.keys(localStorage).forEach(id => {
+    const toDoData = JSON.parse(localStorage.getItem(id));
+    document.body.appendChild(toDoData);
+  });
+
+
+
+});
 document.addEventListener("DOMContentLoaded", function () {
   document.body.addEventListener("click", function (event) {
     const target = event.target;
 
     if (target.classList.contains("button")) {
       const newToDoContainer = document.createElement("div");
+      saveToLocalStorage(newToDoContainer.id, {
+        header: "",
+        tasks: []
+    });
+
+
+
+
+      localStorage.setItem(newToDoContainer.id, JSON.stringify(newToDoContainer));
       newToDoContainer.className = "w-1/4 To-Do bg-slate-700 text-white rounded-md m-5 p-4";
       newToDoContainer.innerHTML = `
         <input class="flex bg-slate-700 text-xl rounded-md justify-center text-center mb-4 w-full" placeholder="To-Do" id="header">
         <div class="delete-button bg-red-500 focus:outline-none block w-1/4 text-center mb-2 p-2 hover:-translate-y-1 duration-300 rounded-md"></div>
         <div class="make-task bg-amber-400 focus:outline-none block w-full text-center hover:-translate-y-1 mb-2 p-2 hover:scale-105 duration-300 rounded-md"></div>
-        <div class="Tasks-container"></div>
-        <div class="delete-green-tasks bg-blue-500 focus:outline-none block w-1/4 text-center mb-2 p-2 hover:-translate-y-1 duration-300 rounded-md"></div>
+        <div class="Tasks-container "></div>
+        <div class=" w-full flex"></div>
+        <div class="delete-green-tasks ml-auto bg-blue-500 justify-end focus:outline-none block w-1/4 text-center mb-2 p-2 hover:-translate-y-1 duration-300 rounded-md"></div>
       `;
 
       document.body.appendChild(newToDoContainer);
 
       const taskContainer = newToDoContainer.querySelector(".Tasks-container");
+
 
       taskContainer.addEventListener("dragstart", function (event) {
         event.dataTransfer.setData("text/plain", event.target.id);
@@ -54,15 +78,35 @@ document.addEventListener("DOMContentLoaded", function () {
             newTask.classList.toggle('bg-green-500');
             newTask.classList.toggle('bg-neutral-800');
           });
-          newTask.addEventListener("", function () {
-            // Remove the clicked task
-            taskContainer.removeChild(newTask);
-          });
-
+          newTask.addEventListener("keydown", function(event) {
+            // Check if the pressed key is the delete key (keycode 46)
+            if (event.keyCode === 46) {
+                // Remove the task if the delete key is pressed
+                taskContainer.removeChild(newTask);
+            }
+        });
+      
           taskContainer.appendChild(newTask);
         } else if (event.target.classList.contains("delete-button")) {
           document.body.removeChild(newToDoContainer);
         }
+        taskContainer.addEventListener("click", e => {
+          // Mark task as complete
+          if (e.target.classList.contains('complete-task')) {
+              const taskId = e.target.parentElement.id;
+              const taskData = JSON.parse(localStorage.getItem(taskId));
+              // Modify taskData.tasks based on the completion status
+              localStorage.setItem(taskId, JSON.stringify(taskData));
+          }
+
+          // Delete to-do
+          if (e.target.classList.contains('delete-button')) {
+              const toDoId = e.target.parentElement.id;
+              localStorage.removeItem(toDoId);
+              e.target.parentElement.remove();
+          }
+
+        });
       });
     }
   });
